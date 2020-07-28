120fc3c109a13ea8730db3cd4f5d65b34ed28b00
Gnanavel <velguru14@gmail.com>
2020-07-28 07:35:40 +0530

    New Git class added

--- Testrep1/DemoTest/PageObject/Git.cs
+++ Testrep1/DemoTest/PageObject/Git.cs
@@ -8,5 +8,22 @@ namespace DemoTest.PageObject
 {
     class Git
     {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "logout")]
+        public IWebElement Logout { get; set; }
+
+
+        public Git(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void logoutPHP()
+        {
+            Logout.Click();
+        }
+
     }
 }
